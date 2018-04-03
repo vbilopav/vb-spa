841bd80b089410bf6a59e04d8e880a3bb0ee093d
Vedran Bilopavlović <vbilopav@gmail.com>
2018-04-03 07:47:37 +0200

    router remove unneccessary

--- src/app/sys/router.js
+++ src/app/sys/router.js
@@ -1,120 +1,118 @@
-define([], () => { 
+define([], () => class {
 
-    return class {
-        constructor(options) {
-            this._navigate = options.navigate || (() => {});
-            this._leave = options.leave || (() => {});
-            this._error = options.error || (() => {});
-            this._hash = options.hash || "#";
-            this._hash === "#" || this._hash === "#!" || (() => {throw this._hash})();
-            this._test = options.test || (route => /^[ A-Za-z0-9_@()/.-]*$/.test(route));
-            let routes = options.routes || (() => {throw options.routes})();
-            this._routes = {};    
-            for(let route in routes) {
-                let data = routes[route];
-                this._test(route) || (() => {throw route})();    
-                this._routes[route] = {
-                    id: data.id || route.replace(/\//g, ""),
-                    name: route,
-                    view: data.view,
-                    paramsMap: data.paramsMap || ((...args) => (args.length === 0 ? {} : false)),
-                    data: data.data
-                }
+    constructor(options) {
+        this._navigate = options.navigate || (() => {});
+        this._leave = options.leave || (() => {});
+        this._error = options.error || (() => {});
+        this._hash = options.hash || "#";
+        this._hash === "#" || this._hash === "#!" || (() => {throw this._hash})();
+        this._test = options.test || (route => /^[ A-Za-z0-9_@()/.-]*$/.test(route));
+        let routes = options.routes || (() => {throw options.routes})();
+        this._routes = {};    
+        for(let route in routes) {
+            let data = routes[route];
+            this._test(route) || (() => {throw route})();    
+            this._routes[route] = {
+                id: data.id || route.replace(/\//g, ""),
+                name: route,
+                view: data.view,
+                paramsMap: data.paramsMap || ((...args) => (args.length === 0 ? {} : false)),
+                data: data.data
             }
-            this._current = undefined; 
-            this._manager = {
-                reveal: ({id,view,params,uri}) => {}, // --> Promise --> elementId
-                leave: (viewId, elementId) => this._manager // --> this
-            };
         }
+        this._current = undefined; 
+        this._manager = {
+            reveal: ({id,view,params,uri}) => {}, // --> Promise --> elementId
+            leave: (viewId, elementId) => this._manager // --> this
+        };
+    }
 
-        useViewManager(
-            manager=(() => {throw manager})()
-        ) {
-            this._manager = manager;
-            return this;
+    useViewManager(
+        manager=(() => {throw manager})()
+    ) {
+        this._manager = manager;
+        return this;
+    }
+
+    start() {
+        this._onChangeEvent(undefined, true);
+        var that = this;
+        window.addEventListener('hashchange', event => {
+            that._onChangeEvent.call(that, event)
+        });
+        return this;
+    }
+
+    getData() {
+        return Object.keys(this._routes).map(name => {
+            let route = this._routes[name],
+                data = route.data || {};                
+            data.url = "/" + this._hash + name;
+            data.id = route.id;    
+            data.active = this._current === route
+            return data;
+        })
+    }
+
+    navigate(location) {
+        document.location.hash = this._hash + location;
+        return this;
+    }
+
+    reveal(location) {
+        this._onChangeEvent({newHash: this._hash + location});
+        return this;
+    }
+
+    _onChangeEvent(event={newHash: document.location.hash}, starting=false) {
+        if (!event.newHash && event.newURL) {
+            event.newHash = event.newURL.replace(document.location.origin + "/", "");
         }
-
-        start() {
-            this._onChangeEvent(undefined, true);
-            var that = this;
-            window.addEventListener('hashchange', event => {
-                that._onChangeEvent.call(that, event)
-            });
-            return this;
-        }
-
-        getData() {
-            return Object.keys(this._routes).map(name => {
-                let route = this._routes[name],
-                    data = route.data || {};                
-                data.url = "/" + this._hash + name;
-                data.id = route.id;    
-                data.active = this._current === route
-                return data;
-            })
-        }
-
-        navigate(location) {
-            document.location.hash = this._hash + location;
-            return this;
-        }
-
-        reveal(location) {
-            this._onChangeEvent({newHash: this._hash + location});
-            return this;
-        }
-
-        _onChangeEvent(event={newHash: document.location.hash}, starting=false) {
-            if (!event.newHash && event.newURL) {
-                event.newHash = event.newURL.replace(document.location.origin + "/", "");
+        let name,
+            uri = event.newHash.replace(this._hash, ""),         
+            uriPieces = uri.split("/").map(item => decodeURIComponent(item)),
+            route, 
+            candidate,    
+            params,
+            test = "";
+            
+        let i, len, sliceIndex;
+        for (i = 0, len = uriPieces.length; i < len; i++) {     
+            let piece = uriPieces[i]; 
+            test = test.endsWith("/") ? test + piece : test + "/" + piece;
+            candidate = this._routes[test];
+            if ((candidate && !route) || (candidate && route.name.length < candidate.name.length)) {
+                route = candidate;                    
+                sliceIndex = i + 1;
             }
-            let name,
-                uri = event.newHash.replace(this._hash, ""),         
-                uriPieces = uri.split("/").map(item => decodeURIComponent(item)),
-                route, 
-                candidate,    
-                params,
-                test = "";
-                
-            let i, len, sliceIndex;
-            for (i = 0, len = uriPieces.length; i < len; i++) {     
-                let piece = uriPieces[i]; 
-                test = test.endsWith("/") ? test + piece : test + "/" + piece;
-                candidate = this._routes[test];
-                if ((candidate && !route) || (candidate && route.name.length < candidate.name.length)) {
-                    route = candidate;                    
-                    sliceIndex = i + 1;
-                }
-            }                              
-            if (route) {
-                if (uriPieces[uriPieces.length - 1] == "") {
-                    uriPieces.splice(-1, 1);
-                }
-                params = route.paramsMap(...uriPieces.slice(sliceIndex));
+        }                              
+        if (route) {
+            if (uriPieces[uriPieces.length - 1] == "") {
+                uriPieces.splice(-1, 1);
             }
+            params = route.paramsMap(...uriPieces.slice(sliceIndex));
+        }
 
-            if (route === undefined || !params) {                
-                this._error({router: this, uri: uri});   
-                return;
-            } 
-                        
-            let viewId, elementId;
-            if (this._current) {
-                viewId = this._current.id;
-                elementId = this._current.elementId;
+        if (route === undefined || !params) {                
+            this._error({router: this, uri: uri});   
+            return;
+        } 
+                    
+        let viewId, elementId;
+        if (this._current) {
+            viewId = this._current.id;
+            elementId = this._current.elementId;
+        }
+        this._manager.leave(viewId, elementId).reveal(
+            {id: route.id, view: route.view, params: params, uri: uri}
+        ).then(elementId => {
+            if (!starting) {                    
+                this._leave({ router: this, route: this._current});
             }
-            this._manager.leave(viewId, elementId).reveal(
-                {id: route.id, view: route.view, params: params, uri: uri}
-            ).then(elementId => {
-                if (!starting) {                    
-                    this._leave({ router: this, route: this._current});
-                }
-                this._current = route;
-                this._current.elementId = elementId;
-                this._navigate({router: this, route: route, params: params})
-            });                        
-        }        
+            this._current = route;
+            this._current.elementId = elementId;
+            this._navigate({router: this, route: route, params: params})
+        });                        
     }
 
  });
