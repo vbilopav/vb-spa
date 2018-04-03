5b0a72a4116d116ad316107211a44ae3e5041e86
Vedran Bilopavlović <vbilopav@gmail.com>
2018-04-03 21:56:44 +0200

    ad node packages

---
+++ postinstall.js
@@ -0,0 +1,8 @@
+var fs = require("fs");
+var uglify = require('uglify-js');
+
+//uglify.minify('./node_modules/requirejs/require.js',)
+
+//cpx.copy('./node_modules/requirejs/bin/r.js', './src/libs/');
+//cpx.copy('./node_modules/requirejs/require.js', './src/libs/');
+//cpx.copy('./node_modules/requirejs-text/text.js', './src/libs/');
