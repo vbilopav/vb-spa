c9564bb823c37bd094814816d7519b5c3137a544
vedran <vedran.bilopavlovic@intis.hr>
2018-04-03 17:19:36 +0200

    add local require

--- src/app/main.js
+++ src/app/main.js
@@ -2,7 +2,7 @@ requirejs.config({
     paths: {
         text: [
             "https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min", 
-            (window._app.libsUrl ? window._app.libsUrl : "") + "text.min"
+            (window._app.libsUrl ? window._app.libsUrl : "") + "text"
         ],
         template: "sys/template"
     }
