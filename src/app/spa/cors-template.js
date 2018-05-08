c27404e6f04b418937b389e569479627a601c7e9
Vedran Bilopavlović <vbilopav@gmail.com>
2018-05-08 07:42:18 +0200

    mode: "no-cors" in cors template

--- src/app/spa/cors-template.js
+++ src/app/spa/cors-template.js
@@ -9,7 +9,7 @@ define([
     return {
         version: '1.0.0',
         load(name, req, onload, config) {
-            fetch(name).then(response => response.text()).then(response => {
+            fetch(name, {mode: "no-cors"}).then(response => response.text()).then(response => {
                 onload((data, locale) => helper.parse(name, response, data, locale))
             })
         }
