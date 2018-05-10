7b2aba1fe5ca18e538a187dd8c4cd08bac10b0a4
Vedran Bilopavlović <vbilopav@gmail.com>
2018-05-10 04:53:51 +0200

    fix typos

--- postinstall.js
+++ postinstall.js
@@ -1,4 +1,4 @@
 var fs = require("fs");
 
 fs.copyFileSync("./node_modules/requirejs/require.js", "./src/libs/require.js");
-fs.copyFileSync("./node_modules//requirejs-text/text.js", "./src/libs/text.js");
+fs.copyFileSync("./node_modules/requirejs-text/text.js", "./src/libs/text.js");
